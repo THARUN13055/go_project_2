2ff1f65ee1dad0216bce4bf918c015f9629314c3
THARUN13055 <megatharun13055>
2023-07-07 13:06:42 +0530

    all

--- go/main.go
+++ go/main.go
@@ -1,32 +1,157 @@
 package main
 
 import (
-	"context"
+	"database/sql"
+	"encoding/json"
 	"fmt"
 	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	_ "github.com/go-sql-driver/mysql"
 )
 
+const (
+	apiPaths = "/apis/v1/books"
+)
+
+type Book struct {
+	Id, Name, Isbn string
+}
+
+type library struct {
+	dbHost, dbPass, dbName , dbUser string
+}
+
+type book struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	ISBN string `json:"isbn"`
+}
+
 func main() {
-	client, err := mongo.NewClient(options.Client().ApplyURI("mongodb+srv://megatharun:W5wWNAnEN7bbPNSd@cluster0.cal26ya.mongodb.net/?retryWrites=true&w=majority"))
-	if err != nil {
-		log.Fatal(err)
+	// DB_HOST is of the form host:port
+
+	dbUser := os.Getenv("DB_USER")
+	if dbUser == "" {
+		dbUser = "root"
 	}
-	ctx, _ := context.WithTimeout(context.Background(), 10*time.Second)
-	err = client.Connect(ctx)
-	if err != nil {
-		log.Fatal(err)
+
+	dbHost := os.Getenv("DB_HOST")
+	if dbHost == "" {
+		dbHost = "localhost:3306"
 	}
-	defer client.Disconnect(ctx)
-	err = client.Ping(ctx, readpref.Primary())
-	if err != nil {
-		log.Fatal(err)
+	// DB_PASS
+	dbPass := os.Getenv("DB_PASS")
+	if dbPass == "" {
+		dbPass = "password"
 	}
-	databases, err := client.ListDatabaseNames(ctx, bson.M{})
-	if err != nil {
-		log.Fatal(err)
+
+	// apiPath
+
+	apiPath := os.Getenv("API_PATH")
+	if apiPath == "" {
+		apiPath = apiPaths
 	}
-	fmt.Println(databases)
+
+	dbName := os.Getenv("DB_NAME")
+	if dbName == "" {
+		dbName = "library"
+	}
+	l := library{
+		dbUser: dbUser,
+		dbHost: dbHost,
+		dbPass: dbPass,
+		dbName: dbName,
+		
+	}
+
+	r := mux.NewRouter()
+	r.HandleFunc(apiPath, l.getBooks).Methods("GET")
+	r.HandleFunc(apiPath, l.createBook).Methods("POST")
+	http.ListenAndServe(":8080", r)
+}
+
+// POST method
+func (l library) createBook(w http.ResponseWriter, r *http.Request) {
+	book := Book{}
+
+	json.NewDecoder(r.Body).Decode(&book)
+
+	db := l.openConnection()
+	defer l.closeConnection(db)
+
+	insertQuery,err := db.Prepare("insert into book values(? ? ? )")
+
+	if err != nil {
+		log.Fatalf("preparing the db qurey %s \n", err.Error())
+	}
+
+	tx, err := db.Begin()
+
+	if err != nil {
+		log.Fatalf("while begining the transaction %s\n", err.Error())
+	}
+
+	_, err = tx.Stmt(insertQuery).Exec(book.Id,book.Name,book.Isbn)
+
+	if err != nil{
+		log.Fatalf("execing the insert commond %s\n",err.Error())
+	}
+	err = tx.Commit()
+	if err != nil{
+		log.Fatalf("while commint the transaction %s\n",err.Error())
+	}
+
+}
+
+// GET Method
+func (l library) getBooks(w http.ResponseWriter, r *http.Request) {
+	db := l.openConnection()
+	defer l.closeConnection(db)
+    
+	rows, err := db.Query("SELECT * FROM book")
+	if err != nil {
+		log.Fatalf("Error querying the database: %s\n", err.Error())
+	}
+
+	defer rows.Close()
+
+	books := []book{}
+
+	for rows.Next() {
+		var id, name, isbn string
+		err := rows.Scan(&id, &name, &isbn)
+		if err != nil {
+			log.Fatalf("Error while scanning the row: %s\n", err.Error())
+		}
+		aBook := book{
+			ID:   id,
+			Name: name,
+			ISBN: isbn,
+		}
+		books = append(books, aBook)
+	}
+
+	json.NewEncoder(w).Encode(books)
 }
 
 
 
+// Connection of db
+func (l library) openConnection() *sql.DB {
+	db, err := sql.Open("mysql", fmt.Sprintf("%s:%s@tcp(%s)/%s", l.dbUser, l.dbPass, l.dbHost, l.dbName))
+	if err != nil {
+		log.Fatalf("Error opening the database: %s\n", err.Error())
+	}
+	return db
+}
+
+// close of db
+func (l library) closeConnection(db *sql.DB) {
+	err := db.Close()
+	if err != nil {
+		log.Fatalf("Error closing the database connection: %s\n", err.Error())
+	}
+}
---
+++ go/script/sql.sh
@@ -0,0 +1,4 @@
+create database library;
+use library;
+create table book (id varchar(100), name varchar(100), isbn varchar(100));
+insert into book values ("1","the every store","ISBN-1");
